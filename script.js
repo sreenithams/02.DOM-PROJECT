4dad6b02bf0d76263b5b0791ca900ef77f1bbeb9
sreenithams <sreenithams05@gmail.com>
2024-10-17 11:41:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*let n=Math.random()
+console.log(n)*/
+
+/*let n=Math.random()
+n=n*6
+console.log(n)*/
+
+/*let n=Math.random()
+n=n*6
+console.log(Math.round(n))*/
+
+/*let n=Math.random()
+n=n*6
+console.log(Math.ceil(n))*/
+
+/*let n=Math.random()
+n=n*6
+console.log(Math.floor(n))*/
+
+
+/*let n=Math.random()
+n=n*100
+console.log(Math.ceil(n))*/
+
+
+let x=prompt("Enter Your Name")
+let y=prompt("Enter Your Friend Name")
+let n=Math.random()
+n= Math.round(n*100)
+alert(`Friendship Strength Between ${x} and ${y} is ${n}%`)
